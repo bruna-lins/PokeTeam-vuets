9760c9adbac5d218c1e7d6b6a6aba60d05101388
= <brunacalipin@gmail.com>
2022-05-26 21:12:16 -0300

    conditional insert to save team button

--- src/components/PokeTeamCard.vue
+++ src/components/PokeTeamCard.vue
@@ -36,6 +36,7 @@
     </div>
     <button type="button"
       class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-1.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      :disabled="store.state.pokeTeam.length <= 3"
       @click="savePokeTeam">
       Save Team
     </button>
