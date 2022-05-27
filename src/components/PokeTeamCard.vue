32d3e23c56490cabf89bef023c49b22f41289094
= <brunacalipin@gmail.com>
2022-05-27 11:32:12 -0300

    working on nickname change

--- src/components/PokeTeamCard.vue
+++ src/components/PokeTeamCard.vue
@@ -21,12 +21,14 @@
               <label for="nickname"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nickname</label>
               <input type="text" id="nickname"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2"
+                v-model="nickname">
               <button type="button"
-                class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-1.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Save</button>
+                class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-1.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                @click="giveNickname(id)">Save</button>
             </div>
             <button type="button"
-              class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-1.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              class="focus:outline text-white bg-red-700 hover:bg-red-800 fring-4 focus:ring-red-300 font-medium roundetext-sm px-5 py-1.5 dark:bg-red-600 daver:bg-red-700 dark:focus:ring-red-900"
               @click="removePokemon(pokemon)">
               Remover
             </button>
@@ -48,13 +50,12 @@ import { Pokemon } from '../interfaces/pokemons';
 import { mutations } from '../store/mutation-types'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 const router = useRouter();
 const store = useStore();
 
 const { REMOVE_FROM_POKETEAM, SET_NICKNAME, SAVE_POKETEAM } = mutations;
-
-// const nickname = ref('')
 
 function getPokemonImg(entryNumber: number): string {
   var str = "" + entryNumber;
@@ -76,10 +77,14 @@ function savePokeTeam() {
   router.push("/team")
 }
 
+// const nickname = ref(store.state.pokeTeam[0].pokemon_species.name)
+const nickname = ref('')
+
+
 // function giveNickname(id: number, name: string) {
 //   let nickname = {
 //     id: id,
-//     name: nickname
+//     name: name
 //   }
 //   store.commit(SET_NICKNAME, nickname)
 //   console.log('I think they liked.')
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,6 +5,7 @@ import api from '../services/api'
 import { mutations } from '../store/mutation-types'
 
 interface State {
+  pokemon: Pokemon[],
   pokeTeam: Pokemon[],
   allTeams: object[]
 }
@@ -15,18 +16,20 @@ const vuexLocal = new VuexPersistence({
 
 const store = new Vuex.Store<State>({
   state: {
+    pokemon: [],
     pokeTeam: [],
     allTeams: []
   },
-  getters: {
-    totalTeam(state) {
-      return state.pokeTeam.length
-    },
-  },
+  // getters: {
+  //   totalTeam(state) {
+  //     return state.pokeTeam.length
+  //   },
+  // },
   mutations: {
     ADD_POKEMON_IN_TEAM(state, payload) {
       state.pokeTeam.push(payload)
     },
+    // CORRIGIR
     REMOVE_FROM_POKETEAM(state, payload) {
       //REMOVENDO TODOS DE UMA VEZ
       // state.pokeTeam = state.pokeTeam.filter((p) => p.id !== payload.id)
@@ -42,8 +45,14 @@ const store = new Vuex.Store<State>({
     DELETE_POKETEAM(state, payload) {
       state.allTeams = state.allTeams.filter((t) => t !== payload)
     },
+    SET_NICKNAME(state, {nickname, id}) {
+      state.pokeTeam = state.pokeTeam.filter(
+        (p) => p.id === id
+      )
+      state.pokeTeam[id].name = nickname
+    }
     // SET_NICKNAME(state, { name, id }) {
-    //   state.pokeTeam[id].name = name
+    //   state.pokemon[id].name = name
     // }
   },
 
